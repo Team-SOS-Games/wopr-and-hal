b9550e308ff281e0aa0295e8b82eed55e169163a
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-02-11 22:47:52 -0500

    some formatting

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     // Initialize collapse sidebar
     $(".button-collapse").sideNav({
-        menuWidth: 300
-        , edge: 'right'
-        ,
+        menuWidth: 300,
+        edge: 'right',
     });
-    
-    $('#chat_send').on('click', function () {
-        $('#textbox').empty();
+
+    $('#chat_send').on('click', function (e) {
+        e.preventDefault();
+
+        $('#textbox').val('');
         console.log("clicked");
-        return false;
     });
 });
