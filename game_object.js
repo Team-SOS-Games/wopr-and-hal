de75cd61113aa0c37b6c10136dea982afd6a69c5
realzaidiguy <realzaidiguy@gmail.com>
2017-02-12 18:06:10 -0500

    game panels and choices working

--- game_object.js
+++ game_object.js
@@ -5,8 +5,8 @@ var game_object = {
 	scenes: [
 		{	
 			scene: 0, 
-			bgImg: "http://www.snorgtees.com/media/catalog/product/c/h/choosewisely_fullpic_artwork.jpg",
-			dialogText: "Make a choice!",
+			bgImg: "https://i.ytimg.com/vi/UYxpX3N20qU/maxresdefault.jpg",
+			dialogText: "Choose wisely. Your life may depend on it!",
 			choices: ["rock", "paper", "scissors"]
 		} // end of scene # 0
 
