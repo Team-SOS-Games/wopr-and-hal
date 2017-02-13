4fa18a480a6d6d1d34f4438b287adccb956b21f0
realzaidiguy <realzaidiguy@gmail.com>
2017-02-12 20:27:39 -0500

    updating user model

--- models/user.js
+++ models/user.js
@@ -1,40 +1,27 @@
 // 'use strict';
 module.exports = function(sequelize, DataTypes) {
+
   var User = sequelize.define('user', {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
-      primaryKey:true
+      primaryKey: true
     },
+
     userName:{
       type: DataTypes.STRING,
       unique:true,
       validate:{
-        notNull:true,
+        isEmpty: false,
         min:1
-      }
-    },
-    email:{
-      type: DataTypes.STRING,
-      min:1,
-      validate:{
-        isEmail:true,
-        notNull:true
       }
     }
   },
   
   {
     timestamps:false
-  },
-  
-  {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    },
-
   });
+
   return User;
+
 };
