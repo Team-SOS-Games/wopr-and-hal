f0dc455b58b48c0a1655bf3f66fe488281631776
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-02-14 00:00:26 -0500

    added Comments throughout code
    Added communication via socket io for user click events on choice buttons

--- public/javascripts/gameChat.js
+++ public/javascripts/gameChat.js
@@ -3,47 +3,57 @@ $(document).ready(function () {
     //grab gameroom number from url on load
     var roomID = Number(window.location.pathname.match(/\/gameroom\/(\d+)$/)[1]);
 
-    console.log(roomID);
+    console.log("chatroom id: " + roomID);
     
     //reference socket io via chat namespace set on server
     var chatIO = io.connect('/chat');
+
     //caching chat components for multiple use 
     var $chat = $('#chat_posts');
     var $postForm = $('#chat_form');
     var $message = $('#chat_message');
 
+    /**
+     * Socket.io custom "on" event listener
+     * runs when user connects to page
+     * @params(string: load) our name for event
+     * @params(obj: data) returns whatever emit('load')
+     * passes ex.{ hello: 'hello from chat' }
+     * @params(emit(Int: roomID)) sends to add user to room
+    */
+    chatIO.on('load', function (data) {
+        console.log(data);
+        chatIO.emit('load', roomID);
+    });
+
+    //submit event for posting in chat
     $postForm.submit(function (e) {
         //keeps form from refreshing page on submit
         e.preventDefault();
 
+        //message obj to hold chat post data
         var message = {
             roomID: roomID,
             msg: $message.val().trim()
         };
 
         //post route test (can be used to add post to DB)
+        //returns status code 200/OK
         $.post('/chat', function (result) {
             console.log(result);
         });
 
-        //send message to io listener on server
-        chatIO.emit('post', message, function (data) {
-            console.log(data);
-        });
+        //send message to "post" io listener on server
+        chatIO.emit('post', message);
+
         //empty chat text box
         $message.val('');
     });
 
-    //renders user posts from server to chat panel 
+    //renders user posts from server to chat panel
     chatIO.on('new post', function (data) {
         console.log(data);
         $chat.append('<li>' + data.msg + '</li>');
-    });
-
-    //runs when user connects to page
-    chatIO.on('load', function (data) {
-        console.log(data);
-        chatIO.emit('load', roomID);
     });
 
 });//end of ready
--- routes/gameRoute.js
+++ routes/gameRoute.js
@@ -1,26 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
+//@params(Obj: io) passed in from server.js
 var gameRouter = function (io) {
 
-    //nice message to test chat route
+    //nice message to test game route
     router.get('/', function (req, res, next) {
         res.send('hello');
     });
 
-    //post route can be used to save post to DB if time allows
+    //post for adding data to leaderboard
     router.post('/', function (req, res, next) {
 
         res.sendStatus(200);
     });
 
-    //when user connects to site this runs
+     /**
+     * when user connects to a page with a io namespace of /game
+     * this will run automatically. All events sent to and from the client
+     * is done inside the connection
+     * @params(String: connection) built in event from socket.io
+     * @params(Obj: socket) this is the client obj created by socket.io
+    */
     io.on('connection', function (socket) {
 
+        //sends to users once connected to page
         socket.emit('load', {game: 'hello from game'});
 
+        /**
+        * listens for user when they connect,
+        * then adds user to a room via socket property
+        * @params(Int: data) room id from client
+        */
         socket.on('load', function(data) {
+            console.log("gameroom id: " + data);
             socket.join(data);
+        });
+
+        /**
+         * Listens for users click events
+         * sends data back to all users
+         * TODO needs to send back to same room only, pass room id from client inside
+         * @params(String: data) id of element clicked by user
+         */
+        socket.on('choice', function(data) {
+            console.log(data);
+            io.emit('choice', { choice: data });
         });
     });
 
