2fd86b7d8459d4ae9791b7686d016d3f9b93d020
realzaidiguy <realzaidiguy@gmail.com>
2017-02-13 20:31:18 -0500

    session storage of username implemented

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,5 +10,13 @@ $(document).ready(function () {
         menuWidth: 300,
         edge: 'right',
     });
+
+    // listen for onclick on lobby submit button
+    $("#text-enter-button").on('click', function() {
+    	// when clicked, grab the text the user entered for username
+    	var localSavedName = $("#enter_text").val().trim();
+    	// save the user name to session storage under key name of sessionUserName
+    	sessionStorage.setItem("sessionUserName", localSavedName);
+    });
     
 });
