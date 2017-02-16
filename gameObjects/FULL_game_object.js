e35d4c2e0321c224b97f068c31fccc050adc79e5
realzaidiguy <realzaidiguy@gmail.com>
2017-02-16 14:11:48 -0500

    fixed sorting by wins on leaderboard

---
+++ gameObjects/FULL_game_object.js
@@ -0,0 +1,26 @@
+// define object for rock/paper/scissors for now
+// array of scenes, bgImg, dialong text, choices
+
+var game_object = {
+	scenes: [
+		{	
+			scene: 0, 
+			bgImg: "https://i.ytimg.com/vi/UYxpX3N20qU/maxresdefault.jpg",
+			dialogText: "Choose wisely. Your life may depend on it!",
+			choices: ["rock", "paper", "scissors"]
+		}, // end of scene # 0
+
+		{	
+			scene: 1, 
+			bgImg: "https://i.ytimg.com/vi/UYxpX3N20qU/maxresdefault.jpg",
+			dialogText: "Choose again! Your life still depends on it!",
+			choices: ["scissors", "rock", "paper"]
+		} // end of scene # 0
+
+	] // end of scenes array
+
+}// end of game_object
+
+// console.log(game_object);
+
+module.exports = game_object;
